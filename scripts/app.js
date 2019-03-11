b5ec15e048d0d9a3df326a56eb03751d70b87c4b
TylerLyczak <tyler.m.lyczak@gmail.com>
2019-03-10 22:11:09 -0500

    Service worker caches page

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,13 @@
+/*
 (function() {
   'use strict';
 
   window.onload = function() {
+
     let message = localStorage.getItem("message") || 'Your message will display here';
     $('#message').html(message);
     $('#display').html(message);
+
   }
 
   $('#button').click(() => {
@@ -21,3 +24,15 @@
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
+*/
+
+window.addEventListener('load', async e =>  {
+  if('serviceWorker' in navigator)  {
+    try {
+      navigator.serviceWorker.register('service-worker.js');
+      console.log('SW Registered');
+    } catch (e) {
+      console.log('SW reg failed');
+    }
+  }
+});
--- service-worker.js
+++ service-worker.js
@@ -1,77 +1,87 @@
-var dataCacheName = 'template-pwa';
-var cacheName = 'template-pwa';
-var filesToCache = [
+const staticAssets = [
   '/',
- "./index.html",
- "./manifest.json",
- "./scripts",
- "./scripts/app.js",
- "./scripts/jquery-3.3.1.js",
- "./scripts/materialize.js",
- "./service-worker.js",
- "./styles",
- "./styles/materialize.css",
- "./styles/style.css",
- "./images",
- "./images/GitHub-Mark-Light-120px-plus.png",
- "./semantic",
- "./semantic/dist",
- "./semantic/dist/semantic.css",
- "./semantic/dist/semantic.min.css",
- "./semantic/dist/semantic.js",
- "./semantic/dist/semantic.min.js",
- "./semantic/dist/themes",
- "./semantic/dist/themes/default",
- "./semantic/dist/themes/default/assets",
- "./semantic/dist/themes/default/fonts",
- "./semantic/dist/themes/default/fonts/brand-icons.eot",
- "./semantic/dist/themes/default/fonts/brand-icons.svg",
- "./semantic/dist/themes/default/fonts/brand-icons.ttf",
- "./semantic/dist/themes/default/fonts/brand-icons.woff",
- "./semantic/dist/themes/default/fonts/brand-icons.woff2",
- "./semantic/dist/themes/default/fonts/icons.eot",
- "./semantic/dist/themes/default/fonts/icons.otf",
- "./semantic/dist/themes/default/fonts/icons.svg",
- "./semantic/dist/themes/default/fonts/icons.ttf",
- "./semantic/dist/themes/default/fonts/icons.woff",
- "./semantic/dist/themes/default/fonts/icons.woff2",
- "./semantic/dist/themes/default/fonts/outline-icons.eot",
- "./semantic/dist/themes/default/fonts/outline-icons.svg",
- "./semantic/dist/themes/default/fonts/outline-icons.ttf",
- "./semantic/dist/themes/default/fonts/outline-icons.woff",
- "./semantic/dist/themes/default/fonts/outline-icons.woff2"
+ 'index.html',
+ 'manifest.json',
+ 'scripts',
+ 'scripts/app.js',
+ 'scripts/jquery-3.3.1.js',
+ 'scripts/materialize.js',
+ 'service-worker.js',
+ 'styles',
+ 'styles/materialize.css',
+ 'styles/style.css',
+ 'images',
+ 'images/GitHub-Mark-Light-120px-plus.png',
+ 'images/icons',
+ 'images/icons/icon-72x72.png',
+ 'images/icons/icon-96x96.png',
+ 'images/icons/icon-128x128.png',
+ 'images/icons/icon-144x144.png',
+ 'images/icons/icon-152x152.png',
+ 'images/icons/icon-192x192.png',
+ 'images/icons/icon-384x384.png',
+ 'images/icons/icon-512x512.png',
+ 'semantic',
+ 'semantic/dist',
+ 'semantic/dist/semantic.css',
+ 'semantic/dist/semantic.min.css',
+ 'semantic/dist/semantic.js',
+ 'semantic/dist/semantic.min.js',
+ 'semantic/dist/themes',
+ 'semantic/dist/themes/default',
+ 'semantic/dist/themes/default/assets',
+ 'semantic/dist/themes/default/assets/fonts',
+ 'semantic/dist/themes/default/assets/fonts/brand-icons.eot',
+ 'semantic/dist/themes/default/assets/fonts/brand-icons.svg',
+ 'semantic/dist/themes/default/assets/fonts/brand-icons.ttf',
+ 'semantic/dist/themes/default/assets/fonts/brand-icons.woff',
+ 'semantic/dist/themes/default/assets/fonts/brand-icons.woff2',
+ 'semantic/dist/themes/default/assets/fonts/icons.eot',
+ 'semantic/dist/themes/default/assets/fonts/icons.otf',
+ 'semantic/dist/themes/default/assets/fonts/icons.svg',
+ 'semantic/dist/themes/default/assets/fonts/icons.ttf',
+ 'semantic/dist/themes/default/assets/fonts/icons.woff',
+ 'semantic/dist/themes/default/assets/fonts/icons.woff2',
+ 'semantic/dist/themes/default/assets/fonts/outline-icons.eot',
+ 'semantic/dist/themes/default/assets/fonts/outline-icons.svg',
+ 'semantic/dist/themes/default/assets/fonts/outline-icons.ttf',
+ 'semantic/dist/themes/default/assets/fonts/outline-icons.woff',
+ 'semantic/dist/themes/default/assets/fonts/outline-icons.woff2'
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open('graffiti-static');
+  cache.addAll(staticAssets);
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if(url.origin == location.origin) {
+    event.respondWith(cacheFirst(req));
+  } else {
+    event.respondWith(networkFirst(req));
+  }
+  event.respondWith(cacheFirst(req));
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+async function cacheFirst(req)  {
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req)  {
+  const cache = await caches.open('graffiti-dynamic');
+
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (e) {
+    return cache.match(req);
+  } finally {
+
+  }
+}
